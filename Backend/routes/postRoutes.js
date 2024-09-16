71ad512148dacf60af7d0900fea06acf2dbc154b
Jose Luis Saez Sanchez <josefus@MacBook-Pro-de-Jose.local>
2024-09-16 09:51:47 +0200

    Arreglando delete

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -2,9 +2,16 @@ const express = require('express');
 const { createPost, getPosts, updatePost, deletePost } = require('../controllers/postController');
 const router = express.Router();
 
-router.post('/', createPost); // Crear entrada
-router.get('/', getPosts); // Obtener entradas
-router.put('/:postId', updatePost); // Editar entrada
-router.delete('/:postId', deletePost); // Eliminar entrada
+// Crear entrada
+router.post('/', createPost);
+
+// Obtener todas las entradas
+router.get('/', getPosts);
+
+// Editar entrada
+router.put('/:id', updatePost);
+
+// Eliminar entrada
+router.delete('/:id', deletePost);
 
 module.exports = router;
